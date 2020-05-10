2868193f40439e2e7a210f96748c1a777d1ae002
Advel0 <0advel0@gmail.com>
2020-05-10 22:41:11 +0300

    Second commit. Everything working nice, but needs refactoring

--- application.py
+++ application.py
@@ -1,6 +1,8 @@
 from time import strftime, localtime
 from flask import Flask, render_template, request, redirect, url_for, session,jsonify
 from flask_socketio import SocketIO, send
+import  json
+
 
 class Channel:
     def __init__(self, name):
@@ -9,6 +11,13 @@ class Channel:
         self.messages=[]
     def add_message(self, message):
         self.messages.append(message)
+    def get_messages(self):
+        messages=[]
+        for message in self.messages:
+            temp = {'username': message.user, 'text': message.text, 'time':message.time}
+            messages.append(temp)
+        return messages
+
 
 class User:
     def __init__(self, name):
@@ -24,52 +33,56 @@ class Message:
         self.text = text
         self.time = time
 
+# Global variables
 channels = [Channel('news'), Channel('test')]
 users = []
 
+# Server settings
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'key'
-
 socketio = SocketIO(app)
 
 # First page. User have to enter thier name or get acces to the dialogues
 @app.route('/')
 def index():
-    if 'user' in session:
+    if 'user_name' in session:
         return redirect(url_for('dialogues'))
-
     return render_template('index.html')
 
 
 #Log out?I believe simple enough
 @app.route('/log-out')
 def log_out():
-    session.pop('user', None)
+    session.pop('user_name', None)
     return redirect(url_for('index'))
 
 # Second PAge. Dialogues. If user tries to get acces without entereing a name he
 # redirects to the start Page
 @app.route('/dialogues', methods=['POST', 'GET'])
 def dialogues():
+    messages = channels[0].get_messages()
+
     if request.method == 'POST':
         name = request.form.get('name')
-        session['user'] = name
-        return render_template('dialogues.html')
+        session['user_name'] = name
+        return render_template('dialogues.html', data=json.dumps(messages))
 
     if request.method == 'GET':
-        if 'user' in session:
-            return render_template('dialogues.html')
-        if len(channels[0].messages) > 0:
-            return redirect(url_for('index', {'messages':channels[0].messages}))
+        if 'user_name' in session:
+            return render_template('dialogues.html', data=json.dumps(messages))
 
     return redirect(url_for('index'))
 
+
+# Resonging on event messsage
 @socketio.on('message')
 def message(data):
+    # Getting variables
     time = strftime('%a-%d %I:%M%p', localtime())
-    message = Message('advel', data, time)
+    message = Message(session['user_name'], data, time)
+    #Creating a message
     channels[0].add_message(message)
-    print(channels[0].name)
+    # Deplo
     send({'message': message.text, 'username': message.user, 'time':time}, broadcast=True)
 if __name__ == '__main__':
     socketio.run(app, Debug=True)
--- static/js/dialogues.js
+++ static/js/dialogues.js
@@ -1,4 +1,19 @@
 document.addEventListener('DOMContentLoaded', ()=>{
+  if (typeof messages !== 'undefined') {
+  console.log('exist')
+  messages.forEach(message => {
+    const p = document.createElement('p');
+    const br = document.createElement('br');
+    const span_username = document.createElement('span');
+    const time_span = document.createElement('span');
+    span_username.innerHTML = message['username'];
+    time_span.innerHTML = message['time'];
+    p.innerHTML = span_username.outerHTML+ br.outerHTML +time_span.outerHTML + br.outerHTML +message['text'];
+    document.querySelector('#messages-field').append(p);
+  })
+} else {
+  console.log('not exist')
+}
   var socket = io.connect(location.protocol + '//' + document.domain + ':' + location.port);
 
   document.querySelector('#send-message').onclick = ()=>{
@@ -6,13 +21,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
     socket.send(text);
     document.querySelector('#message').value = '';
   }
-  socket.on('connect', (data)=>{
-    if (data){
-      for (message in data.messages){
-        document.querySelector('#messages-field').append('1')
-      }
-  }
-  })
+
   socket.on('message', data => {
     const p = document.createElement('p');
     const br = document.createElement('br');
